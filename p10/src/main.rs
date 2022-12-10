68309ba5e94d452fa609676efead77705461736a
olegr <oryjkov@gmail.com>
2022-12-10 12:43:43 +0100

    day 10
    playing with an iterator adaptor

--- p10/src/main.rs
+++ p10/src/main.rs
@@ -1,7 +1,22 @@
 use std::fs;
+#[derive(Debug)]
 enum Instr {
     Nop,
     Add(i32),
+}
+impl Instr {
+    fn len(&self) -> usize {
+        match self {
+            Instr::Nop => 1,
+            Instr::Add(_) => 2,
+        }
+    }
+    fn apply(&self, x: &mut i32) {
+        match self {
+            Instr::Add(i) => *x += i,
+            _ => {}
+        }
+    }
 }
 
 #[cfg(test)]
@@ -14,7 +29,50 @@ mod tests {
     }
 }
 
-fn make_states(f: &str) -> Vec<i32> {
+struct State {
+    instr: Instr,
+    sub_i: usize,
+    x: i32,
+}
+struct StateMachine<I> {
+    iter: I,
+    state: State,
+}
+impl<I> Iterator for StateMachine<I>
+where
+    I: Iterator<Item = Instr>,
+{
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.state.sub_i < self.state.instr.len() {
+            self.state.sub_i += 1;
+            let old_x = self.state.x;
+            self.state.instr.apply(&mut self.state.x);
+            Some(old_x)
+        } else if let Some(instr) = self.iter.next() {
+            self.state.instr = instr;
+            self.state.sub_i = 1;
+            Some(self.state.x)
+        } else {
+            None
+        }
+    }
+}
+trait StateMachineIterator: Iterator + Sized {
+    fn process_instrs(self) -> StateMachine<Self> {
+        StateMachine {
+            iter: self,
+            state: State {
+                instr: Instr::Nop,
+                sub_i: 1,
+                x: 1,
+            },
+        }
+    }
+}
+impl<I: Iterator<Item = Instr>> StateMachineIterator for I {}
+
+fn parse_input(f: &str) -> Vec<Instr> {
     fs::read_to_string(f)
         .expect("read fail")
         .split('\n')
@@ -27,56 +85,42 @@ fn make_states(f: &str) -> Vec<i32> {
                 Instr::Add(i[1].parse().unwrap())
             }
         })
-        .map(|i| match i {
-            Instr::Nop => vec![0],
-            Instr::Add(x) => vec![0, x],
-        })
-        .into_iter()
-        .reduce(|mut a, mut b| {
-            a.append(&mut b);
-            a
-        })
-        .unwrap()
+        .collect()
 }
 
 fn solve_p1(f: &str) -> i32 {
-    let xs = make_states(f);
-    let mut x = 1;
-    let mut sum = 0;
-    for (i, cycle) in xs.iter().enumerate() {
-        let cycle_num = i + 1;
-        if cycle_num % 40 == 20 {
-            sum += cycle_num as i32 * x;
-            println!("{cycle_num}: {}", cycle_num as i32 * x);
-        }
-        x += cycle;
-    }
-    sum
+    parse_input(f)
+        .into_iter()
+        .process_instrs()
+        .enumerate()
+        .filter(|(cycle, _)| cycle % 40 == 20)
+        .map(|(cycle, x)| x * cycle as i32)
+        .sum()
 }
 
 fn solve_p2(f: &str) {
-    let xs = make_states(f);
-    let mut x = 1;
-    for (i, cycle) in xs.iter().enumerate() {
-        let cycle_num = i + 1;
-        let col = i % 40;
-        let d = if (col as i32 - x).abs() <= 1 {
-            "#"
-        } else {
-            "."
-        };
-        print!("{d}");
-        if col == 39 {
-            println!()
-        }
-        x += cycle;
-    }
+    parse_input(f)
+        .into_iter()
+        .process_instrs()
+        .enumerate()
+        .map(|(cycle, x)| {
+            let col = cycle % 40;
+            let d = if (col as i32 - x).abs() <= 1 {
+                "#"
+            } else {
+                "."
+            };
+            print!("{d}");
+            if col == 39 {
+                println!()
+            }
+        })
+        .count();
 }
 
 fn main() {
     let f = "input.txt";
-    let sum = solve_p1(f);
-    println!("part1: {sum}");
+    println!("part1: {}", solve_p1(f));
     println!("part2:");
     solve_p2(f);
 }
