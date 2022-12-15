890df970efb61f104da98f062af471f56f82c157
olegr <oryjkov@gmail.com>
2022-12-15 10:51:40 +0100

    day15 part1

---
+++ p15/src/main.rs
@@ -0,0 +1,117 @@
+use gcollections::ops::*;
+use interval::interval_set::*;
+use interval::ops::*;
+use interval::Interval;
+use std::fs;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_calc() {
+        assert_eq!(
+            to_iset(
+                &vec![SB {
+                    sensor: (1, 0),
+                    beacon: (2, 0)
+                }],
+                2
+            )
+            .size(),
+            0
+        );
+        assert_eq!(
+            to_iset(
+                &vec![SB {
+                    sensor: (1, 0),
+                    beacon: (2, 0)
+                }],
+                1
+            )
+            .size(),
+            1
+        );
+    }
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve_part1("test.txt", 10), 26);
+        assert_eq!(solve_part1("input.txt", 10), 4725496);
+    }
+    #[test]
+    fn test_part2() {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct SB {
+    // Coordinates as x,y
+    sensor: (i32, i32),
+    beacon: (i32, i32),
+}
+fn to_iset(inp: &Vec<SB>, ml: i32) -> IntervalSet<i32> {
+    inp.iter()
+        .map(|sb| {
+            let budget = (sb.sensor.0 - sb.beacon.0).abs() + (sb.sensor.1 - sb.beacon.1).abs();
+            let dy = (ml - sb.sensor.1).abs();
+            let rv = (sb.sensor.0 - (budget - dy), sb.sensor.0 + (budget - dy));
+            println!("budget: {}, dy: {}, i: {:?}, sb: {:?}", budget, dy, rv, sb);
+            rv
+        })
+        .filter(|i| i.1 >= i.0)
+        .map(|i| vec![i].to_interval_set())
+        .fold(IntervalSet::empty(), |set, interval| set.union(&interval))
+}
+
+fn calc(inp: &Vec<SB>, ml: i32) -> i32 {
+    let i = to_iset(inp, ml);
+    let out = inp
+        .iter()
+        .filter(|sb| sb.beacon.1 == ml)
+        .map(|sb| IntervalSet::singleton(sb.beacon.0))
+        .fold(IntervalSet::empty(), |set, interval| set.union(&interval));
+    i.difference(&out).size() as i32
+}
+fn solve_part1(f: &str, ml: i32) -> i32 {
+    calc(
+        &fs::read_to_string(f)
+            .unwrap()
+            .lines()
+            .filter(|l| l.len() > 0)
+            .map(|l| {
+                let mut sx = 0i32;
+                let mut sy = 0i32;
+                let mut bx = 0i32;
+                let mut by = 0i32;
+                scanf::sscanf!(
+                    l,
+                    "Sensor at x={}, y={}: closest beacon is at x={}, y={}",
+                    sx,
+                    sy,
+                    bx,
+                    by
+                )
+                .unwrap();
+                SB {
+                    sensor: (sx, sy),
+                    beacon: (bx, by),
+                }
+            })
+            .collect(),
+        ml,
+    )
+}
+
+fn solve_part2(f: &str) -> i32 {
+    /*
+    fs::read_to_string(f)
+        .unwrap()
+        .lines()
+        .count();
+     */
+    -1
+}
+
+fn main() {
+    println!("part 1: {}", solve_part1("input.txt", 2_000_000));
+    println!("part 2: {}", solve_part2("input.txt"));
+}
