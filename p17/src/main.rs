cedc17699b40ba05e0d8b08491b579247fe91b48
olegr <oryjkov@gmail.com>
2022-12-17 14:29:36 +0100

    day 17 part 2 too slow
    will take 130k seconds to solve

--- p17/src/main.rs
+++ p17/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::VecDeque, fs, time::{Instant, Duration}};
 
 #[cfg(test)]
 mod test {
@@ -10,7 +10,9 @@ mod test {
         assert_eq!(solve_part1("input.txt"), 3224);
     }
     #[test]
-    fn test_part2() {}
+    fn test_part2() {
+        //assert_eq!(solve_part2("test.txt"), 1514285714288);
+    }
 }
 
 fn rocks() -> Vec<Rock> {
@@ -41,10 +43,10 @@ fn rocks() -> Vec<Rock> {
     ]
 }
 
-const NUM_ROCKS : usize = 2022;
-const N2 : usize = 1000000000000;
+const NUM_ROCKS: usize = 2022;
+const N2: usize = 1_000_000_000_000;
 
-fn solve_part1(f: &str) -> i32 {
+fn solve(f: &str, n: usize) -> i64 {
     let jets = fs::read_to_string(f)
         .unwrap()
         .lines()
@@ -57,8 +59,9 @@ fn solve_part1(f: &str) -> i32 {
     let rocks = rocks();
     let mut field = Field::new();
     let mut jets_iter = jets.iter().cycle();
-    for (n, rock) in rocks.iter().cycle().enumerate() {
-        if n >= NUM_ROCKS {
+    let start = Instant::now();
+    for (rock_num, rock) in rocks.iter().cycle().enumerate() {
+        if rock_num >= n {
             break;
         }
         field.grow(rock.height() + 3);
@@ -66,12 +69,12 @@ fn solve_part1(f: &str) -> i32 {
         let mut top_y = 0;
         for offset in &mut jets_iter {
             // show the rock
-            field.try_place(rock, top_x, top_y).show();
+            //field.try_place(rock, top_x, top_y).show();
 
             if field.check(rock, top_x + offset, top_y) {
                 top_x += offset;
                 // show the rock
-                field.try_place(rock, top_x, top_y).show();
+                //field.try_place(rock, top_x, top_y).show();
             }
             let should_stop = !field.check(rock, top_x, top_y + 1);
             if should_stop {
@@ -81,20 +84,27 @@ fn solve_part1(f: &str) -> i32 {
         }
 
         field.place(rock, top_x, top_y);
-        field.show();
+        //field.show();
+        //field.show();
+        if field.rows.len() > 1_000_000 {
+            field.shrink_bottom();
+        }
+        if rock_num % 10_000_000 == 1_000_000 {
+            let mrps = (rock_num as f64) / start.elapsed().as_secs_f64() / 1e6;
+            let will_take = Duration::from_secs_f64(((n-rock_num) as f64 / mrps) / 1e6);
+            println!("{}, Mrps: {:.2}, will take: {:?}", rock_num, mrps, will_take );
+        }
     }
-    field.height() as i32
+    field.height() as i64
 }
 
-fn solve_part2(f: &str) -> i32 {
-    fs::read_to_string(f).unwrap().lines().count();
-    -1
+fn solve_part1(f: &str) -> i64 {
+    solve(f, NUM_ROCKS)
+}
+fn solve_part2(f: &str) -> i64 {
+    solve(f, N2)
 }
 
-#[derive(Clone)]
-struct Field {
-    rows: Vec<[u8; 9]>,
-}
 #[derive(Clone, Debug)]
 struct Rock {
     bm: Vec<Vec<u8>>,
@@ -120,17 +130,24 @@ fn cell_empty(cell: u8) -> bool {
     cell == b'.'
 }
 
+#[derive(Clone)]
+struct Field {
+    rows: Vec<[u8; 9]>,
+    cleared: usize,
+}
+
 const EMPTY_ROW: [u8; 9] = [b'|', b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'|'];
 const BOTTOM_ROW: [u8; 9] = [b'+', b'-', b'-', b'-', b'-', b'-', b'-', b'-', b'+'];
 impl Field {
     fn new() -> Self {
         Field {
             rows: vec![BOTTOM_ROW],
+            cleared: 0,
         }
     }
 
     fn height(&self) -> usize {
-        self.rows.len() - 1
+        self.rows.len() - 1 + self.cleared
     }
     // Checks if placing a rock at x and with top at y is valid.
     fn check(&self, rock: &Rock, top_x: isize, top_y: usize) -> bool {
@@ -177,6 +194,21 @@ impl Field {
 
     fn grow(&mut self, n: usize) {
         (0..n).for_each(|_| self.rows.push(EMPTY_ROW));
+    }
+    fn shrink_bottom(&mut self) {
+        let mut can_clear = self.rows.len();
+        let mut b = vec![];
+        for col in 0..7 {
+            let mut y = self.rows.len() - 1;
+            while cell_empty(self.rows[y][col + 1]) {
+                y -= 1;
+            }
+            can_clear = can_clear.min(y);
+            b.push(y)
+        }
+        //println!( "max: {}, {:?}, can clear {} bottom rows", can_clear, b, can_clear);
+        self.rows = self.rows.split_off(can_clear);
+        self.cleared += can_clear;
     }
     fn shrink(&mut self) {
         while self.rows[self.rows.len() - 1] == EMPTY_ROW {
