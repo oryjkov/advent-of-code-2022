6041d9450a290bc64727cfd5c5ee0cb7e8093394
olegr <oryjkov@gmail.com>
2022-12-25 11:03:35 +0100

    day 24 part 1 and 2

--- p24/src/main.rs
+++ p24/src/main.rs
@@ -1,20 +1,9 @@
 use std::fs;
-use std::ops::{Index, IndexMut};
-use std::str::from_utf8;
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test_apply() {
-        use Dir::*;
-        assert_eq!(Right.apply(Pos(1, 1), Pos(1, 1)), None);
-        assert_eq!(Down.apply(Pos(2, 2), Pos(2, 2)), None);
-        assert_eq!(Up.apply(Pos(2, 2), Pos(2, 2)).unwrap(), Pos(1, 2));
-        assert_eq!(Left.apply(Pos(2, 2), Pos(2, 2)).unwrap(), Pos(2, 1));
-        assert_eq!(Same.apply(Pos(2, 2), Pos(2, 2)).unwrap(), Pos(2, 2));
-    }
     #[test]
     fn test_bliz() {
         let b = Bliz::new(2, 1, 5);
@@ -23,49 +12,15 @@ mod test {
         assert!(b.at(0, -3));
     }
     #[test]
-    fn test_at() {
-        #[rustfmt::skip]
-         let inp = [
-            ">..".as_bytes(),
-            "...".as_bytes(),
-            "...".as_bytes(),
-        ];
-        let m = Map::parse(&inp);
-        assert_eq!(m.at(0, Pos(2, 2)), [0; 5]);
-        assert_eq!(m.at(0, Pos(0, 0)), [1, 0, 0, 0, 0]);
-        assert_eq!(m.at(1, Pos(0, 0)), [0, 0, 0, 0, 1]);
-        assert_eq!(m.at(1, Pos(1, 1)), [0, 1, 0, 0, 0]);
-
-        #[rustfmt::skip]
-        let inp = [
-            ".v.".as_bytes(),
-            "...".as_bytes(),
-            "...".as_bytes(),
-        ];
-        println!("input 2");
-        let m = Map::parse(&inp);
-        assert_eq!(m.at(0, Pos(0, 0)), [0, 0, 0, 0, 1]);
-        assert_eq!(m.at(1, Pos(1, 1)), [1, 0, 0, 0, 0]);
-        assert_eq!(m.at(0, Pos(1, 1)), [0, 1, 0, 0, 0]);
-
-        #[rustfmt::skip]
-        let inp = [
-            ".v..".as_bytes(),
-            "<.<.".as_bytes(),
-            ".^..".as_bytes(),
-        ];
-        println!("input 2");
-        let m = Map::parse(&inp);
-        assert_eq!(m.at(0, Pos(0, 0)), [0, 0, 0, 1, 1]);
-        assert_eq!(m.at(1, Pos(1, 1)), [3, 0, 0, 0, 0]);
-        assert_eq!(m.at(0, Pos(1, 1)), [0, 1, 1, 1, 1]);
-    }
-    #[test]
     fn test_part1() {
-        //assert_eq!(solve_part1("test.txt"), _);
+        assert_eq!(solve_part1("test.txt"), 18);
+        assert_eq!(solve_part1("input.txt"), 301);
     }
     #[test]
-    fn test_part2() {}
+    fn test_part2() {
+        assert_eq!(solve_part2("test.txt"), 54);
+        assert_eq!(solve_part2("input.txt"), 859);
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -95,63 +50,6 @@ struct Map {
     cols: Vec<Vec<Bliz>>,
 }
 
-#[derive(Copy, Clone, Debug)]
-enum Dir {
-    Same = 0,
-    Up = 1,
-    Left = 2,
-    Down = 3,
-    Right = 4,
-}
-impl Dir {
-    fn apply(&self, p: Pos, max_pos: Pos) -> Option<Pos> {
-        use Dir::*;
-        match self {
-            Same => Some(p),
-            Up => {
-                if p.0 == 0 {
-                    None
-                } else {
-                    Some(Pos(p.0 - 1, p.1))
-                }
-            }
-            Left => {
-                if p.1 == 0 {
-                    None
-                } else {
-                    Some(Pos(p.0, p.1 - 1))
-                }
-            }
-            Down => {
-                if p.0 == max_pos.0 {
-                    None
-                } else {
-                    Some(Pos(p.0 + 1, p.1))
-                }
-            }
-            Right => {
-                if p.1 == max_pos.1 {
-                    None
-                } else {
-                    Some(Pos(p.0, p.1 + 1))
-                }
-            }
-        }
-    }
-}
-impl<T> Index<Dir> for [T; 5] {
-    type Output = T;
-    fn index(&self, r: Dir) -> &T {
-        &self[r as usize]
-    }
-}
-
-impl<T> IndexMut<Dir> for [T; 5] {
-    fn index_mut(&mut self, r: Dir) -> &mut T {
-        &mut self[r as usize]
-    }
-}
-
 impl Map {
     fn height(&self) -> usize {
         self.rows.len()
@@ -166,7 +64,6 @@ impl Map {
         let mut rows = vec![vec![]; 2 + height as usize];
         let mut cols = vec![vec![]; 2 + width as usize];
         inp.iter().enumerate().for_each(|(row_num, in_row)| {
-            //println!("#{}#", from_utf8(in_row).unwrap());
             in_row
                 .iter()
                 .enumerate()
@@ -189,92 +86,39 @@ impl Map {
         m
     }
     fn bottom_right(&self) -> Pos {
-        Pos(self.rows.len() - 3, self.cols.len() - 3)
-    }
-    fn at_hor(&self, rows: &[usize], n: usize, c: usize, rv: &mut [usize; 5]) {
-        for bliz in self.rows[rows[0]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Up] += 1;
-            }
-        }
-        for bliz in self.rows[rows[2]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Down] += 1;
-            }
-        }
-        for bliz in self.rows[rows[1]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Same] += 1;
-            } else if bliz.at(n, c as isize - 1) {
-                rv[Dir::Left] += 1;
-            } else if bliz.at(n, c as isize + 1) {
-                rv[Dir::Right] += 1;
-            }
-        }
-    }
-    fn at_ver(&self, cols: &[usize], n: usize, c: usize, rv: &mut [usize; 5]) {
-        for bliz in self.cols[cols[0]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Left] += 1;
-            }
-        }
-        for bliz in self.cols[cols[2]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Right] += 1;
-            }
-        }
-        for bliz in self.cols[cols[1]].iter() {
-            if bliz.at(n, c as isize) {
-                rv[Dir::Same] += 1;
-            } else if bliz.at(n, c as isize - 1) {
-                rv[Dir::Up] += 1;
-            } else if bliz.at(n, c as isize + 1) {
-                rv[Dir::Down] += 1;
-            }
-        }
+        Pos(self.rows.len() - 2, self.cols.len() - 2)
     }
     fn at1(&self, n: usize, p: Pos) -> bool {
-        for bliz in &self.rows[p.0 + 1] {
-            if bliz.at(n, p.1 as isize) {
+        for bliz in &self.rows[p.0] {
+            if bliz.at(n, p.1 as isize - 1) {
                 return false;
             }
         }
-        for bliz in &self.cols[p.1 + 1] {
-            if bliz.at(n, p.0 as isize) {
+        for bliz in &self.cols[p.1] {
+            if bliz.at(n, p.0 as isize - 1) {
                 return false;
             }
         }
         true
     }
-
-    fn at(&self, n: usize, p: Pos) -> [usize; 5] {
-        //println!("at {:?}", p);
-        let mut rv = [0; 5];
-        let (row, col) = (p.0, p.1);
-
-        self.at_ver(&[col, col + 1, col + 2], n, row, &mut rv);
-        //println!("at ver: {:?}", rv);
-        self.at_hor(&[row, row + 1, row + 2], n, col, &mut rv);
-        //println!("together: {:?}", rv);
-
-        rv
-    }
 }
 
-type Path = Vec<Dir>;
-
-fn solve2(m: &Map) -> usize {
+fn solve2(m: &Map, starting_n: usize, from: Pos, to: Pos) -> usize {
     let mut can_be = vec![vec![vec![10000; m.width()]; m.height()]; 1];
     let mut n = 1;
     loop {
         can_be.push(vec![vec![10000; m.width()]; m.height()]);
-        can_be[n][1][1] = if m.at1(n, Pos(0, 0)) { n } else { 10000 };
+        can_be[n][from.0][from.1] = if m.at1(n + starting_n - 1, from) {
+            n
+        } else {
+            10000
+        };
         for r in 1..(m.height() - 1) {
             for c in 1..(m.width() - 1) {
-                if r == 1 && c == 1 {
+                if Pos(r, c) == from {
                     continue;
                 }
-                can_be[n][r][c] = if m.at1(n, Pos(r - 1, c - 1)) {
+                can_be[n][r][c] = if m.at1(n + starting_n - 1, Pos(r, c)) {
                     [
                         can_be[n - 1][r][c],
                         can_be[n - 1][r - 1][c],
@@ -312,69 +156,13 @@ fn solve2(m: &Map) -> usize {
             }
             println!();
         }
-        println!("checking in {n},{},{} = {}", r, c, can_be[n][r][c]);
          */
-        let r = m.height() - 2;
-        let c = m.width() - 2;
-        if can_be[n][r][c] < 10000 {
+        if can_be[n][to.0][to.1] < 10000 {
             break;
         }
         n = n + 1;
     }
-    n+1
-}
-
-fn solve(m: &Map) -> usize {
-    let mut path = vec![];
-    let mut min_len = 100000;
-    loop {
-        let n = path.len();
-        let neibs = m.at(n + 1, Pos(0, 0));
-        if neibs[Dir::Down] == 0 {
-            println!("starting at n={n}");
-            walk(m, &mut path, &mut min_len, Pos(0, 0));
-            println!("starting at n={n}, min len: {}", min_len);
-        }
-        path.push(Dir::Same);
-        if n > 5 {
-            break;
-        }
-    }
-    min_len + 1
-}
-
-fn walk(m: &Map, path: &mut Path, min_len: &mut usize, pos: Pos) -> bool {
-    use Dir::*;
-    let n = path.len();
-    let br = m.bottom_right();
-
-    let steps_remaining = (br.0 - pos.0) + (br.1 - pos.1);
-    if n + steps_remaining >= *min_len {
-        return false;
-    }
-
-    if pos == br {
-        *min_len = n;
-        println!("len: {}", path.len());
-        return true;
-    }
-    let dirs = if br.1 - pos.1 > br.0 - pos.0 {
-        [Right, Down, Same, Up] //, Left]
-    } else {
-        [Down, Right, Same, Left] //, Left, Up]
-    };
-    let neibs = m.at(n + 1, pos);
-    for dir in dirs {
-        if neibs[dir] > 0 {
-            continue;
-        }
-        if let Some(new_pos) = dir.apply(pos, m.bottom_right()) {
-            path.push(dir);
-            walk(m, path, min_len, new_pos);
-            path.pop();
-        }
-    }
-    return false;
+    n + starting_n
 }
 
 fn read_input(f: &str) -> Map {
@@ -399,16 +187,21 @@ fn read_input(f: &str) -> Map {
 
 fn solve_part1(f: &str) -> usize {
     let m = read_input(f);
-    solve2(&m)
+    solve2(&m, 1, Pos(1, 1), m.bottom_right())
 }
 
-fn solve_part2(f: &str) -> i32 {
-    fs::read_to_string(f).unwrap().lines().count();
-    -1
+fn solve_part2(f: &str) -> usize {
+    let m = read_input(f);
+    let n1 = 1;
+    let n1 = solve2(&m, n1, Pos(1, 1), m.bottom_right());
+    let n1 = solve2(&m, n1, m.bottom_right(), Pos(1, 1));
+    let n1 = solve2(&m, n1, Pos(1, 1), m.bottom_right());
+    n1
 }
 
 fn main() {
-    //println!("part 1: {}", solve_part1("test.txt"));
+    println!("part 1: {}", solve_part1("test.txt"));
     println!("part 1: {}", solve_part1("input.txt"));
-    //println!("part 2: {}", solve_part2("input.txt"));
+    println!("part 2: {}", solve_part2("test.txt"));
+    println!("part 2: {}", solve_part2("input.txt"));
 }
